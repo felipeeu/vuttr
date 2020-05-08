ee6a673fb5759949e45ce6e359721e61352a3758
Felipe Domingues <>
2020-05-08 09:57:54 -0300

    implement service layer

--- services/config.js
+++ services/config.js
@@ -1,14 +1,8 @@
 import axios from "axios";
+
 class ConfigService {
   static endPointBaseUrl = `https://localhost:3000/`;
 
-  /**
-   * Receives a object and transpile it to a query parameter string
-   * @param {obj} props Object with the key & value that will be transpiler to a query parameter string
-   * @returns Query parameter string for a GET request
-   */
-
- 
 
   static publicHeaders = () => {
     const params = {
@@ -17,14 +11,7 @@ class ConfigService {
     return params;
   };
 
-  /**
-   * Create a instance with the api information's
-   * @param {string} url URL path
-   * @param {obj} data Object containing the key and value that will be passed do the URL query param
-   * @returns Get url ready to be requested
-   */
- 
-
+  
   static publicApiEndpoint = () => {
     const instance = axios.create({
       baseURL: ConfigService.endPointBaseUrl,
