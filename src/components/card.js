6561f333bed76463e9fbe91a2c706c0384f6d254
Felipe Domingues <>
2020-05-08 11:39:34 -0300

    fetching data to show in cards

--- src/components/card.js
+++ src/components/card.js
@@ -1,18 +1,50 @@
-import React from 'react'
-import styled from 'styled-components'
-
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import ToolsService from "../services/tools";
 
 const CardNote = styled.section.attrs(props => ({}))`
- align-self:center;
- width: 30em;
- height: 10em;
- background-color:red;
+  align-self: center;
+  width: 30em;
+  height: 10em;
+  background-color: red;
+`;
+
+const Title = styled.a.attrs(props => ({}))`
+  background-color: purple;
+`;
+
+const Description = styled.p.attrs(props => ({}))`
+  background-color: green;
+`;
+
+const Tags = styled.p.attrs(props => ({}))`
+  background-color: yellowgreen;
+`;
+const RemoveButton = styled.p.attrs(props => ({}))`
+  background-color: yellow;
 `;
 
 const Card = () => {
-  return (
-    <CardNote> Notes </CardNote>
-  )
-}
+  const [toolData, setToolData] = useState([]);
 
-export default Card
+  useEffect(() => {
+    const request = ToolsService.getTools();
+    request.then(response => setToolData(response.data));
+  }, []);
+
+  return (
+    toolData &&
+    toolData.map(item => (
+      <CardNote>
+        <Title>{item.title}</Title>
+        <Description>{item.description}</Description>
+        {item.tags.map(tag => (
+          <Tags>{`# ${tag}`}</Tags>
+        ))}
+        <RemoveButton>remove</RemoveButton>
+      </CardNote>
+    ))
+  );
+};
+
+export default Card;
