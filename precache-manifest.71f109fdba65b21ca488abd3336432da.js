4dc12766a41397382900eb6fa4b652696a1a47b6
Felipe Domingues <>
2020-05-13 09:49:40 -0300

    Updates

---
+++ precache-manifest.71f109fdba65b21ca488abd3336432da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01afd3409bf6f69ecf1ed99e6d0c7e85",
+    "url": "/vuttr/index.html"
+  },
+  {
+    "revision": "98b9c0ad96a1c3a7f852",
+    "url": "/vuttr/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a1eb5f3e0c8423c15fcb",
+    "url": "/vuttr/static/js/2.6b9b3293.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/vuttr/static/js/2.6b9b3293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e00353f207b1d321e2e",
+    "url": "/vuttr/static/js/3.65c5d312.chunk.js"
+  },
+  {
+    "revision": "98b9c0ad96a1c3a7f852",
+    "url": "/vuttr/static/js/main.290fb29d.chunk.js"
+  },
+  {
+    "revision": "24a9ad61f6dcfe77c831",
+    "url": "/vuttr/static/js/runtime-main.6eb24dfc.js"
+  }
+]);
